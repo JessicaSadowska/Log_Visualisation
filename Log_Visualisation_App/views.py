ebce9ef0a796367d2d0e051d9290f2643ff5f579
kotlowska@student.agh.edu.pl <kotlowska@student.agh.edu.pl>
2023-04-02 14:59:40 +0200

    initial drawings

--- Log_Visualisation_App/views.py
+++ Log_Visualisation_App/views.py
@@ -3,7 +3,9 @@ import codecs
 from django.shortcuts import render
 from django.views import View
 import ocel
+import json
 from django.core.files.storage import FileSystemStorage
+# from .forms import ObjectForm
 
 
 class Home(View):
@@ -21,9 +23,19 @@ class Home(View):
             'data': ocel_log.get_events(),
             'objects_count': ocel_log.objects_count(),
             'objects': ocel_log.get_objects(),
+            'event_names_string': ocel_log.get_events_names_string(),
+            'event_names_array': ocel_log.get_events_names_array()
         }
         return render(request, 'uploaded.html', context)
     
+    # def get_events_with_objects(request):
+    #     if request.method == 'POST':
+    #         form = ObjectForm(request.POST)
+    #         objects = "sdf"
+    #     else:
+    #         form = ObjectForm()
+
+    #     return render(request, 'uploaded.html', {'form': form, 'objects': objects})
 
 class OcelLog:
     def __init__(self, log):
@@ -31,10 +43,30 @@ class OcelLog:
 
     def get_events(self):
         return ocel.get_events(self.log)
+    
+    def get_events_names_string(self):
+        events = ocel.get_events(self.log)
+        return ', ' .join(list(events.keys()))
+    
+    def get_events_names_array(self):
+        events = ocel.get_events(self.log)
+        return list(events.keys())
 
     def get_objects(self):
         return ocel.get_objects(self.log)
 
     def objects_count(self):
         return len(ocel.get_objects(self.log))
+                           
+    def my_filtering_function(pair):
+        pass
+        # unwanted_key = 'Matt'
+        # key, value = pair
+        # if key == unwanted_key:
+        #     return False  
+        # else:
+        #     return True
+        
+    def get_events_with_objects(self):
+        pass 
     
